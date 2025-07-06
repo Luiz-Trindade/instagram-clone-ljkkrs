bcdead15a75e0ea668f3bf0972a85f4f417cd8c0
Luiz-Trindade <luiz.gabriel.m.trindade@gmail.com>
2025-07-06 14:25:36 -0300

    fix: fixed icons tab

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <ion-app style="margin-top: 5px;">
+  <ion-app>
     <ion-router-outlet />
   </ion-app>
 </template>
 
 <script setup lang="ts">
+import { StatusBar, Style } from '@capacitor/status-bar';
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  // Oculta a status bar (toolbar do Android)
+  await StatusBar.setOverlaysWebView({ overlay: true });
+  await StatusBar.setStyle({ style: Style.Light }); // Opcional: define o estilo
+});
 </script>
 
 <style>
 .fade-in {
-  animation: fadeIn 1.5s ease-in-out;
+  animation: fadeIn .5s ease-in-out;
 }
 
 @keyframes fadeIn {
